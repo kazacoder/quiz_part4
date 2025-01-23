f80e734b09fb6930078b28573297316eb0ec569d
kazacoder <avtk.tlt@gmail.com>
2025-01-23 18:06:43 +0300

    JS to TS rewriting started. webpack configured, tsc initiated. app.js rewrote .js removed from imports

--- frontend/src/components/choice.ts
+++ frontend/src/components/choice.ts
@@ -1,4 +1,4 @@
-import {UrlManager} from "../utils/url-manager.js";
+import {UrlManager} from "../utils/url-manager";
 import {CustomHttp} from "../services/custom-http";
 import config from "../../config/config";
 import {Auth} from "../services/auth";
